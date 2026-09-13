28f69d13e2e635fbce7792ab991d52dfbea2bf69
xhyxin <3161649035@qq.com>
2026-09-14 01:51:38 +0800

    1.7测试

--- js/data.js
+++ js/data.js
@@ -3528,30 +3528,6 @@ window.APP_DATA = {
       },
       "source": "manual",
       "personality": "gloomy"
-    },
-    {
-      "id": "youngchun",
-      "name": "咏春",
-      "en": "youngchun",
-      "group": "韩服3星",
-      "personality": "resonance",
-      "aliases": [
-        "youngchun",
-        "口香糖"
-      ],
-      "img": "assets/chars/Youngchun.png",
-      "fav": {
-        "love": [
-          "F67"
-        ],
-        "like": [
-          "F22"
-        ],
-        "hate": [
-          "F25"
-        ]
-      },
-      "source": "manual"
     }
   ],
   "foods": [
